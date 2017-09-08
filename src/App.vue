737e6f1cdf25b5543b9216e13d79170a38f041f9
Joe Marriott <joe@j-marriott.com>
2017-09-08 12:42:36 +0100

    first quote data passed

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="container">
-
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
     export default {
         data: function () {
-          quotes: [
-            'This is a quote'
-          ]
+          return {
+            quotes: [
+              'This is a quote'
+            ],
+          }
           maxQuotes: 10
+        },
+        components: {
+          appQuoteGrid: QuoteGrid
         }
-    }
+      }
 </script>
 
 <style>
